b4b4200cf6d9ef0bfbb48fe366980f13a15b0e05
rafal <rafalgondek0@gmail.com>
2026-03-15 17:25:13 +0100

    header fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Car } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
       <div className="h-14 w-full bg-gray-200">
         <div className="flex items-center justify-between max-w-7xl h-full mx-auto text-lg">
-          <div className="flex items-center justify-center text-xl cursor-pointer">
-            <Car className={"text-blue-600"} />
-            <div className={"font-bold"}>RentFlow</div>
-          </div>
+          <Link href="/">
+            <div className="flex items-center justify-center text-xl cursor-pointer">
+              <Car size={32} className={"text-blue-600"} />
+              <div className={"font-bold"}>RentFlow</div>
+            </div>
+          </Link>
           <div
             className={"flex items-center justify-center gap-6 text-slate-600"}
           >
@@ -24,9 +28,14 @@ const Header = () => {
             <p className={"cursor-pointer hover:text-blue-600"}>FAQ</p>
             <p className={"cursor-pointer hover:text-blue-600"}>Kontakt</p>
           </div>
-          <div className={""}>
-            <p>Zaloguj</p>
-          </div>
+          <Button
+            asChild
+            className={
+              "bg-slate-900 p-4 text-base text-white hover:bg-slate-700 transition-all duration-150"
+            }
+          >
+            <Link href="/">Zaloguj</Link>
+          </Button>
         </div>
       </div>
     </>
